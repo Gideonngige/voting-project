b96762c80326771fa77f8335904e9248959868c0
Gideonngige <ushindigideon01@gmail.com>
2024-11-22 11:00:33 +0300

    added backend for login.php and register.php

--- login.php
+++ login.php
@@ -13,13 +13,44 @@
         <div class="inner-container">
             <h2>Login</h2><hr>
             <form action="login.php" method="POST">
-                <label for="username">Registration Number</label><br>
-                <input type="text" id="username" name="regno" class="form-control" required><br>
                 <label for="password">ID Number</label><br>
                 <input type="password" id="idnumber" class="form-control" name="idnumber" required><br>
+                <label for="username">Registration Number</label><br>
+                <input type="text" id="username" name="regno" class="form-control" required><br>
                 <a href="#">Forgot your password?</a>
                 <input type="submit" class="form-control" value="Login">
             </form>
+            <?php 
+            $servername = "localhost";
+            $username = "root";
+            $password = "@mysql2024";
+            $dbname = "election";
+            $conn = mysqli_connect($servername, $username, $password, $dbname);
+             if(!$conn){
+                die("Connection failed!" . mysqli_connect_error());
+             }
+             else{
+               echo "Connected";
+             }
+             if(isset($_POST["regno"]) && isset($_POST["idnumber"])){
+                $regno = $_POST["regno"];
+                $idnumber = $_POST["idnumber"];
+                $sql = "SELECT first_name, last_name, id_number, reg_number FROM Voters WHERE reg_number='$regno' AND id_number='$idnumber'";
+                $result = mysqli_query($conn, $sql);
+                if(mysqli_num_rows($result) > 0){
+                    while($row = mysqli_fetch_assoc($result)){
+                        session_start();
+                        $_SESSION["username"] = $row["first_name"] . " " . $row["last_name"];
+                        header("Location: home.php");
+                    }
+                    echo '<div class="alert alert-success" role="alert">'; echo "Login Succesful"; echo '</div>';
+                } else{
+                    echo '<div class="alert alert-danger" role="alert">'; echo "Invalid registration number or ID number"; echo '</div>';
+                }
+             }else{
+                echo '<div class="alert alert-danger" role="alert">'; echo "Provide all inputs to login"; echo '</div>'; 
+             }
+            ?>
             </div>
 
     </div>
--- register.php
+++ register.php
@@ -15,12 +15,12 @@
             <form action="register.php" method="POST">
                 <table>
                     <tr>
-                        <td><label>First Name<br><input type="text" name="fname" class="form-control" /></label></td>
-                        <td><label>Last Name<br><input type="text" name="lname" class="form-control" /></label></td>
+                        <td><label>First Name<br><input type="text" name="fname" class="form-control" required/></label></td>
+                        <td><label>Last Name<br><input type="text" name="lname" class="form-control" required /></label></td>
                     </tr>
                 </table>
                 <label for="county">County</label><br>
-                <select name="county" class="form-control">
+                <select name="county" class="form-control" required>
                     <option value="">Select County</option>
                     <?php
                         $counties = array("Lamu", "Tana River", "Mombasa", "Kilifi");
@@ -31,8 +31,49 @@
                 </select>
                 <label for="password">ID Number</label><br>
                 <input type="password" id="password" class="form-control" name="idnumber" required><br>
-                <input type="submit" class="form-control" value="Register">
+                <a href="login.php">Already registered? login here</a>
+                <input type="submit" class="form-control" value="Register"><br>
+                
             </form>
+
+            <?php 
+            $servername = "localhost";
+            $username = "root";
+            $password = "@mysql2024";
+            $dbname = "election";
+            $conn = mysqli_connect($servername, $username, $password, $dbname);
+             if(!$conn){
+                //die("Connection failed!" . mysqli_connect_error());
+             }
+             else{
+               // echo "Connected";
+             }
+
+             if(isset($_POST["fname"]) && isset($_POST["lname"]) && isset($_POST["county"]) && isset($_POST["idnumber"])){
+                $fname = $_POST["fname"];
+                $lname = $_POST["lname"];
+                $county = $_POST["county"];
+                $idnumber = $_POST["idnumber"];
+                $sql1 = "SELECT id_number FROM Voters WHERE id_number = '$idnumber' ";
+                $result = mysqli_query($conn, $sql1);
+                if(mysqli_num_rows($result) > 0){
+                    echo '<div class="alert alert-danger" role="alert">'; echo "You are already registered"; echo '</div>'; 
+                }
+                else{
+                    $reg_number =strtolower($lname . substr($idnumber, -4));
+                    $sql2 = "INSERT INTO Voters (first_name, last_name, id_number, reg_number) VALUES ('$fname', '$lname','$idnumber', '$reg_number');";
+                    if(mysqli_query($conn, $sql2)){
+                        echo '<div class="alert alert-success" role="alert">'; echo "Registration successful<br>Your registration number is " . $reg_number; echo '</div>';
+                    }
+                    else{
+                        echo "Error: ". $sql2. "<br>". mysqli_error($conn);
+                    }
+                }
+             }else{
+                echo '<div class="alert alert-danger" role="alert">'; echo "Please provide all the inputs!! "; echo '</div>';
+
+             }
+            ?>
             </div>
 
     </div>
